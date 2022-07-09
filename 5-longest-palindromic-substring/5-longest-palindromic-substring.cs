23e2180b1de495170ced51f7b3173df9cb078ce5
manyeonglee <57290419+manyeonglee@users.noreply.github.com>
2022-07-09 12:39:20 +0900

    Time: 166 ms (51.97%), Space: 36.3 MB (90.58%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public string LongestPalindrome(string s) {
+        if(s=="" || s.Length<=1)
+            return s;
+        int length = 0, start = 0;
+        for(int i=0; i<s.Length; i++)
+        {
+            int currentLength = PalindromeLength(s, i, i+1);
+            int oddLength = PalindromeLength(s, i, i);
+            currentLength = Math.Max(currentLength, oddLength);
+            
+            if(currentLength>length)
+            {
+                length = currentLength;
+                start = i-(length-1)/2;
+            }
+        }
+        
+        return s.Substring(start, length);
+    }
+    
+    private int PalindromeLength(string s, int left, int right){
+        while(left>=0 && right<s.Length && s[left] == s[right])
+        {
+            left--;
+            right++;
+        }
+        return right-left-1; // (right+1) - (left-1) - 1
+    }
+}
